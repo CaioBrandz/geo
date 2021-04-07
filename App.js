fb5c10337781cb01507d2a39c6c1a0925f529fdf
Caio B <caiobrandz@gmail.com>
2021-04-07 16:22:40 -0300

    geolocalizacao

--- App.js
+++ App.js
@@ -1,11 +1,69 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button, PermissionsAndroid, Platform } from 'react-native';
+import Geolocation from '@react-native-community/geolocation';
+Geolocation.setRNConfiguration(config);
 
 export default function App() {
+  const [currentLatitude, setCurrentLatitude] = useState('');
+  const [currentLongitude, setCurrentLongitude] = useState('');
+  const [watchID, setWatchID] = useState(0);
+
+  const callLocation = () =>{
+    if(Platform.OS=== 'ios'){
+      getLocation();
+    } else{
+      const requestLocationPermission = async () =>{
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.ACESS_FINE_LOCATION,
+          {
+            title:"permissao",
+            message: "acesso a localização",
+            buttonNeutral: "pergunte dps",
+            buttonPositive:"ok",
+            buttonNegative:"cancelar"
+          }
+        );
+        if(granted === PermissionsAndroid.RESULTS.GRANTED){
+          getLocation();
+        } else {
+          alert('Permissao negada');
+        }
+      };
+      requestLocationPermission();
+    }
+  }
+
+  const getLocation = () =>{
+    Geolocation.getCurrentPosition(
+      (position) => {
+        const currentLatitude = JSON.stringify(position.coords.latitude);
+        const currentLongitude = JSON.stringify(position.coords.longitude);
+        setCurrentLatitude(currentLatitude);
+        setCurrentLongitude(currentLongitude);
+      },
+      (error) => alert(error.message),
+      { enableHighAccuracy:true,timeout:20000, maximumAge:1000}
+    );
+    const watchID = Geolocation.watchPosition((position) => {
+      const currentLatitude = JSON.stringify(position.coords.latitude);
+      const currentLongitude = JSON.stringify(position.coords.longitude);
+      setCurrentLatitude(currentLatitude);
+      setCurrentLongitude(currentLongitude);
+    });
+    setWatchID(watchID);
+  }
+
+  const clearLocation= () =>{
+    Geolocation.clearWatch(watchID);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Latitude: {currentLatitude}</Text>
+      <Text>Longitude: {currentLongitude}</Text>
+      <View><Button title="obter" onPress={callLocation}/></View>
+      <View><Button title="cancel" onPress={clearLocation}/></View>
       <StatusBar style="auto" />
     </View>
   );
